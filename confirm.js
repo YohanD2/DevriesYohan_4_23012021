ea1678d12334cd1f0bf8ca6a93899062b4eeaf57
YohanD2 <yohan.devries@jvais.net>
2021-06-22 22:54:29 +0200

    delete last js file

--- confirm.js
+++
@@ -1,8 +0,0 @@
-// Get url's parameter and display it
-
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const orderId = urlParams.get('id_order')
-
-let spanOrderId = document.getElementById('orderId');
-spanOrderId.textContent = orderId;
